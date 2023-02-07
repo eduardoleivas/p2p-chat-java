d90c36508971a64ec112c43c5c7014d95c60016a
eduardoleivas <120347105+eduardoleivas@users.noreply.github.com>
2023-02-07 19:37:44 -0300

    Update CChatController.java

--- mcnchat2/src/main/java/cliente/util/CChatController.java
+++ mcnchat2/src/main/java/cliente/util/CChatController.java
@@ -43,13 +43,10 @@ public class CChatController {
     private int msg = 0;
     private int dist = 20;
     private boolean firstMsg = true;
-
+    
+    //INICIALIZA OS RECURSOS NECESSÁRIOS
     public void initialize() throws IOException {
-        //INICIALIZA OS RECURSOS NECESSÁRIOS
         client = ConnectionFactory.getChat(); //INICIALIZA O SOCKET
-        System.out.println("FXML STARTED = Conectado com sucesso!");
-        System.out.println(client.getInetAddress() + " | " + client.getPort());
-        System.out.println(client.getRemoteSocketAddress() + " | " + client.getLocalSocketAddress());
         dIn = new DataInputStream(client.getInputStream());
         dOut = new DataOutputStream(client.getOutputStream());
         Thread serverListener = new Thread(() -> {
@@ -59,15 +56,14 @@ public class CChatController {
                     String serverMsg = dIn.readUTF(); //LÊ A MENSAGEM DO INPUTSTREAM (CHARSET UTF)
                     if (!serverMsg.equals(lastMsg)) {
                         lastMsg = serverMsg;
-                        //CASO O SERVIDOR ENVIE /QUIT
-
+                        
+                        //CASO O SERVIDOR RETORNE O USERNAME
                         if (serverMsg.contains("/username")) {
                             String user[] = serverMsg.split(",");
                             svUser = user[1];
 
-                            //CASO O SERVIDOR ENVIE /QUIT
+                        //CASO O SERVIDOR ENVIE /QUIT
                         } else if (serverMsg.equals("/quit")) {
-                            System.out.println("Conexão Encerrada.");
                             dIn.close();
                             dOut.close();
                             client.close();
@@ -80,7 +76,6 @@ public class CChatController {
                             });
                             break;
                         } else {
-                            System.out.println("Server: " + serverMsg);
                             Platform.runLater(new Runnable() {
                                 @Override
                                 public void run() {
@@ -91,7 +86,7 @@ public class CChatController {
                     }
                 }
             } catch (IOException e) {
-                //e.printStackTrace();
+                e.printStackTrace();
             }
         });
         serverListener.start();
@@ -101,6 +96,7 @@ public class CChatController {
         clUser = username;
     }
 
+    //ADICIONA A MENSAGEM RECEBIDA PELO CONTATO COMO LABEL NA BOX DO CHAT
     private void svAddLabel(String txt) {
         setClUser(((Stage) lblBase.getScene().getWindow()).getTitle());
         Label lblMsg = new Label(svUser + ": " + txt);
@@ -112,6 +108,7 @@ public class CChatController {
         msg++;
     }
 
+    //ADICIONA A MENSAGEM ENVIADA PARA O CONTATO COMO LABEL NA BOX DO CHAT
     private void clAddLabel(String txt) {
         setClUser(((Stage) lblBase.getScene().getWindow()).getTitle());
         Label lblMsg = new Label(clUser + ": " + txt);
@@ -123,11 +120,10 @@ public class CChatController {
         msg++;
     }
 
-    @FXML
+    @FXML //MÉTODO FECHAR CONEXÃO
     void closeConnection(ActionEvent event) throws IOException {
         String quit = "/quit";
         dOut.writeUTF(quit); //ESCREVE MENSAGEM NO OUTPUTSTREAM (CHARSET UTF)
-        System.out.println("Conexão Encerrada.");
         dIn.close();
         dOut.close();
         client.close();
@@ -135,7 +131,7 @@ public class CChatController {
         chat.close();
     }
 
-    @FXML
+    @FXML //MÉTODO ENVIAR MENSAGEM
     void sendMessage(ActionEvent event) {
         try {
             if(firstMsg) {
@@ -145,7 +141,6 @@ public class CChatController {
             //CLIENTE ENVIA A MENSAGEM
             String clientMsg = txtField.getText();
             dOut.writeUTF(clientMsg); //ESCREVE MENSAGEM NO OUTPUTSTREAM (CHARSET UTF)
-            System.out.println(clientMsg);
             clAddLabel(clientMsg);
             txtField.clear();
         } catch (IOException e) {
@@ -153,12 +148,12 @@ public class CChatController {
         }
     }
 
+    //ENVIA A INFO DO USUÁRIO PARA O CONTATO
     void sendUser() {
         try {
             //SERVIDOR ENVIA A MENSAGEM
             String clientMsg = ((Stage) lblBase.getScene().getWindow()).getTitle();
             dOut.writeUTF("/username," + clientMsg); //ESCREVE A MENSAGEM NO OUTPUT STREAM (CHARSET UTF)
-            System.out.println("Username: " + clientMsg);
         } catch (IOException e) {
             e.printStackTrace();
         }
