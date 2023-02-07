1f878deeda5e430ecbb82b8b015f14cdd8e900e5
eduardoleivas <120347105+eduardoleivas@users.noreply.github.com>
2023-02-07 20:14:36 -0300

    Update ServerThread.java

--- mcnchat2/src/main/java/servidor/util/ServerThread.java
+++ mcnchat2/src/main/java/servidor/util/ServerThread.java
@@ -17,7 +17,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
+//SERVERTHREAD OBJECT EXTENDS THREAD
 public class ServerThread extends Thread {
+    
     String line = null;
     BufferedReader is = null;
     PrintWriter os = null;
@@ -25,11 +27,13 @@ public class ServerThread extends Thread {
     Connection conn = null;
     Usuario user = null;
 
+    //SERVERTHREAD OBJECT CREATOR
     public ServerThread(Socket s) {
         s1 = s;
         this.conn = dbManager.dbConnect();
     }
 
+    //ONRUN METHOD
     public void run() {
         try {
             is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
@@ -41,17 +45,23 @@ public class ServerThread extends Thread {
                     }
                 }
             server.delClient(user.getUsername());
+            
             try {
                 dbManager.dbStatusUpdate(conn, 0, user.getUsername());
+                
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
+            
             killAll();
             this.destroy();
+            
         } catch(IOException | SQLException ie) {
             server.delClient(user.getUsername());
+            
             try {
                 dbManager.dbStatusUpdate(conn, 0, user.getUsername());
+                
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -60,20 +70,21 @@ public class ServerThread extends Thread {
         }
     }
 
+    //MESSAGE PRE-PROCCESSING METHOD
     private void checkMsg(String msg) throws SQLException {
-        //COMANDO GETFRIENDS
+        //COMMAND GETFRIENDS
         if(msg.contains("/getFriends:")) {
             try {
                 String[] msgContent = msg.split(":");
                 String username = msgContent[1];
                 int id = user.getId_user();
-                System.out.println("ID getFriends: " + id);
                 List<Usuario> amigos = dbManager.dbGetFriends(this.conn, "id_amigo", "contatos", "id_user", id);
                 int size = amigos.size();
                 String resp = "/loadFriends:";
+                
                 if(amigos.size() != 0) {
                     for(int i=0; i<size; i++) {
-                        Usuario temp = amigos.get(i); //RECEBE O USUARIO NA POSIÇÃO X
+                        Usuario temp = amigos.get(i); //STORES THE USER AT POSITION X
                         resp = resp.concat(temp.getUsername() + ";");
                     }
                 }
@@ -81,16 +92,20 @@ public class ServerThread extends Thread {
                 os.flush();
                 resp = is.readLine();
                 line = resp;
+                
             } catch (IOException e) {
                 try {
                     server.delClient(user.getUsername());
+                    
                     try {
                         dbManager.dbStatusUpdate(conn, 0, user.getUsername());
+                        
                     } catch (SQLException e2) {
                         throw new RuntimeException(e2);
                     }
                     killAll();
                     throw new RuntimeException(e);
+                    
                 } catch (IOException ex) {
                     server.delClient(user.getUsername());
                     throw new RuntimeException(ex);
@@ -98,16 +113,16 @@ public class ServerThread extends Thread {
             }
         }
 
-        //COMANDO GETONLINEFRIENDS
+        //COMMAND GETONLINEFRIENDS
         if(msg.contains("/getOnlineFriends:")) {
             try {
                 String[] msgContent = msg.split(":");
                 String username = msgContent[1];
                 int id = user.getId_user();
-                System.out.println("ID getOnlineFriends: " + id);
                 List<Usuario> amigos = dbManager.dbGetOnlineFriends(this.conn, id);
                 int size = amigos.size();
                 String resp = "/loadOnlineFriends:";
+                
                 for(int i=0; i<size; i++) {
                     Usuario temp = amigos.get(i); //RECEBE O USUARIO NA POSIÇÃO X
                     resp = resp.concat(temp.getUsername() + ";");
@@ -116,12 +131,14 @@ public class ServerThread extends Thread {
                 os.flush();
                 resp = is.readLine();
                 line = resp;
+                
             } catch (IOException e) {
                 try {
                     server.delClient(user.getUsername());
                     dbManager.dbStatusUpdate(conn, 0, user.getUsername());
                     killAll();
                     throw new RuntimeException(e);
+                    
                 } catch (IOException ex) {
                     server.delClient(user.getUsername());
                     throw new RuntimeException(ex);
@@ -129,7 +146,7 @@ public class ServerThread extends Thread {
             }
         }
 
-        //COMANDO LOGINREQUEST
+        //COMMAND LOGINREQUEST
         if(msg.contains("/loginRequest:")) {
             if(user == null) {
                 try {
@@ -140,9 +157,11 @@ public class ServerThread extends Thread {
                     boolean login = dbManager.dbLogin(this.conn, username, pass);
                     System.out.println(login);
                     String resp;
+                    
                     if(login == true) {
                         resp = "/loginStatus:1";
                         dbManager.dbStatusUpdate(conn, 1, username);
+                        
                     } else {
                         resp = "/loginStatus:0";
                     }
@@ -152,11 +171,13 @@ public class ServerThread extends Thread {
                     os.flush();
                     resp = is.readLine();
                     line = resp;
+                    
                 } catch (IOException e) {
                     try {
                         server.delClient(user.getUsername());
                         killAll();
                         throw new RuntimeException(e);
+                        
                     } catch (IOException ex) {
                         server.delClient(user.getUsername());
                         throw new RuntimeException(ex);
@@ -169,10 +190,12 @@ public class ServerThread extends Thread {
                     os.flush();
                     resp = is.readLine();
                     line = resp;
+                    
                 } catch (IOException e) {
                     try {
                         killAll();
                         throw new RuntimeException(e);
+                        
                     } catch (IOException ex) {
                         server.delClient(user.getUsername());
                         throw new RuntimeException(ex);
@@ -181,24 +204,25 @@ public class ServerThread extends Thread {
             }
         }
 
-        //COMANDO GETUSERID
+        //COMMAND GETUSERID
         if(msg.contains("/userIdRequest:")) {
             try {
                 String[] msgContent = msg.split(":");
                 String username = msgContent[1];
                 int id = dbManager.dbGetIdByUsername(this.conn, "id_user", "usuario", "username", username);
-                System.out.println(id);
                 user.setId_user(id);
                 String resp = "/userIdReturn:" + id;
                 os.println(resp);
                 os.flush();
                 resp = is.readLine();
                 line = resp;
+                
             } catch (IOException e) {
                 try {
                     server.delClient(user.getUsername());
                     killAll();
                     throw new RuntimeException(e);
+                    
                 } catch (IOException ex) {
                     server.delClient(user.getUsername());
                     throw new RuntimeException(ex);
@@ -206,7 +230,7 @@ public class ServerThread extends Thread {
             }
         }
 
-        //COMANDO REQUESTFRIENDCONNECTION
+        //COMMAND REQUESTFRIENDCONNECTION
         if(msg.contains("/requestFriendConnection:")) {
             try {
                 // -- SPLIT STRING CONTENTS -- //
@@ -225,7 +249,7 @@ public class ServerThread extends Thread {
                 });
                 connect.start();
                 String resp = "1";
-                System.out.println("FCS: " + foundFriend[0]);
+                
                 // -- SEND ANSWER AND FLUSH INPUTSTREAM -- //
                 os.println(resp);
                 os.flush();
@@ -236,21 +260,22 @@ public class ServerThread extends Thread {
                     server.delClient(user.getUsername());
                     killAll();
                     throw new RuntimeException(e);
+                    
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
             }
         }
 
-        //COMANDO DELCLIENT
+        //COMMAND DELCLIENT
         if(msg.contains("/delClient:")) {
+            
             // -- SPLIT STRING CONTENTS -- //
             String[] msgContent = msg.split(":");
             server.delClient(msgContent[1]);
-            System.out.println(msg);
-                String resp = "/clientDeleted:1";
-                os.println(resp);
-                os.flush();
+            String resp = "/clientDeleted:1";
+            os.println(resp);
+            os.flush();
             try {
                 resp = is.readLine();
             } catch (IOException e) {
@@ -268,16 +293,16 @@ public class ServerThread extends Thread {
         server.addClients(user.getUsername(), ip, this);
     }
 
+    //METHOD SEND MESSAGE
     public void sendMsg(String ip, String port) throws IOException {
         String resp = "/connectToUserServer:"+ip+";"+port;
-        System.out.println(resp);
         os.println(resp);
         os.flush();
         resp = is.readLine();
         line = resp;
     }
 
-    //KILL ALL
+    //METHOD KILL ALL RESOURCES
     private void killAll() throws IOException, SQLException {
         is.close();
         os.close();
